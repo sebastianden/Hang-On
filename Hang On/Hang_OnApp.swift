730f1a008b7468f38fc74b6a5e6714e1d1504ee6
STDG <stdg@novonordisk.com>
2025-04-21 16:42:54 +0200

    Initial Commit

---
+++ Hang On/Hang_OnApp.swift
@@ -0,0 +1,17 @@
+//
+//  Hang_OnApp.swift
+//  Hang On
+//
+//  Created by STDG (Sebastian Dengler) on 21.04.25.
+//
+
+import SwiftUI
+
+@main
+struct Hang_OnApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Hang OnTests/Hang_OnTests.swift
@@ -0,0 +1,17 @@
+//
+//  Hang_OnTests.swift
+//  Hang OnTests
+//
+//  Created by STDG (Sebastian Dengler) on 21.04.25.
+//
+
+import Testing
+@testable import Hang_On
+
+struct Hang_OnTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
